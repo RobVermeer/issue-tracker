df89f41c173c9be51390b793d9df64fc4166d95e
RobVermeer <rob@vermeertech.nl>
2023-12-22 23:54:15 +0100

    Make issue scrollable

--- src/components/Issues/index.tsx
+++ src/components/Issues/index.tsx
@@ -21,6 +21,7 @@ import {
 } from "../ui/sheet"
 import { Button } from "../ui/button"
 import { Separator } from "../ui/separator"
+import { ScrollArea } from "../ui/scroll-area"
 
 interface Props {
   projectId?: string
@@ -77,61 +78,63 @@ export async function Issues({ projectId }: Props) {
                 <Activity className="text-primary" /> Issue
               </SheetTitle>
             </SheetHeader>
-            <dl className="divide-y divide-gray-100">
-              <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                <dt className="text-sm font-medium leading-6 text-foreground">
-                  Project
-                </dt>
-                <dd className="mt-1 text-sm leading-6 text-muted-foreground sm:col-span-2 sm:mt-0">
-                  <Badge variant="outline">{project.name}</Badge>
-                </dd>
-              </div>
-              <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                <dt className="text-sm font-medium leading-6 text-foreground">
-                  Message
-                </dt>
-                <dd className="mt-1 text-sm leading-6 text-muted-foreground sm:col-span-2 sm:mt-0">
-                  {message}
-                </dd>
-              </div>
-              <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                <dt className="text-sm font-medium leading-6 text-foreground">
-                  Created at
-                </dt>
-                <dd className="mt-1 text-sm leading-6 text-muted-foreground sm:col-span-2 sm:mt-0">
-                  {new Date(createdAt).toLocaleDateString("en", {
-                    month: "long",
-                    day: "numeric",
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    hour12: false,
-                  })}
-                </dd>
-              </div>
-              <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                <dt className="text-sm font-medium leading-6 text-foreground">
-                  Level
-                </dt>
-                <dd className="mt-1 text-sm leading-6 text-muted-foreground sm:col-span-2 sm:mt-0">
-                  <Badge variant="secondary">{level}</Badge>
-                </dd>
-              </div>
+            <ScrollArea className="h-full pb-8">
+              <dl className="divide-y divide-gray-100">
+                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                  <dt className="text-sm font-medium leading-6 text-foreground">
+                    Project
+                  </dt>
+                  <dd className="mt-1 text-sm leading-6 text-muted-foreground sm:col-span-2 sm:mt-0">
+                    <Badge variant="outline">{project.name}</Badge>
+                  </dd>
+                </div>
+                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                  <dt className="text-sm font-medium leading-6 text-foreground">
+                    Message
+                  </dt>
+                  <dd className="mt-1 text-sm leading-6 text-muted-foreground sm:col-span-2 sm:mt-0">
+                    {message}
+                  </dd>
+                </div>
+                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                  <dt className="text-sm font-medium leading-6 text-foreground">
+                    Created at
+                  </dt>
+                  <dd className="mt-1 text-sm leading-6 text-muted-foreground sm:col-span-2 sm:mt-0">
+                    {new Date(createdAt).toLocaleDateString("en", {
+                      month: "long",
+                      day: "numeric",
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      hour12: false,
+                    })}
+                  </dd>
+                </div>
+                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                  <dt className="text-sm font-medium leading-6 text-foreground">
+                    Level
+                  </dt>
+                  <dd className="mt-1 text-sm leading-6 text-muted-foreground sm:col-span-2 sm:mt-0">
+                    <Badge variant="secondary">{level}</Badge>
+                  </dd>
+                </div>
 
-              {extra &&
-                Object.entries(JSON.parse(extra)).map(([key, value]) => (
-                  <div
-                    key={key}
-                    className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0"
-                  >
-                    <dt className="text-sm font-medium leading-6 text-foreground">
-                      {key}
-                    </dt>
-                    <dd className="mt-1 text-sm leading-6 text-muted-foreground sm:col-span-2 sm:mt-0">
-                      {value as string}
-                    </dd>
-                  </div>
-                ))}
-            </dl>
+                {extra &&
+                  Object.entries(JSON.parse(extra)).map(([key, value]) => (
+                    <div
+                      key={key}
+                      className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0"
+                    >
+                      <dt className="text-sm font-medium leading-6 text-foreground">
+                        {key}
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-muted-foreground sm:col-span-2 sm:mt-0">
+                        {value as string}
+                      </dd>
+                    </div>
+                  ))}
+              </dl>
+            </ScrollArea>
           </SheetContent>
         </Sheet>
       ))}
